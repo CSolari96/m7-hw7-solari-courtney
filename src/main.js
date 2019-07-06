8212545c86b56329f0cd0edbfe66434d4649e28e
Courtney Solari <csolari@ufl.edu>
2019-07-06 16:47:18 -0500

    Store ice cream image and caption in variables

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// Store ice cream image element
+const iceCream = document.getElementsByTagName("img")[0];
+
+// Store image caption element
+const caption = document.getElementsByClassName("caption")[0];
