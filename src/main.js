21b2e3634d9fcc97664718704539365964cfe225
Courtney Solari <csolari@ufl.edu>
2019-07-06 16:58:01 -0500

    Add event listeners and function for image hover

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ const iceCream = document.getElementsByTagName("img")[0];
 
 // Store image caption element
 const caption = document.getElementsByClassName("caption")[0];
+
+// Toggle .hide class
+const toggleHide = () => caption.classList.toggle("hide");
+
+// When the user's mouse enters or leaves the image, toggle the display of the caption
+iceCream.addEventListener("mouseenter", toggleHide);
+iceCream.addEventListener("mouseleave", toggleHide);
