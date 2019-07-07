084bc9509a1bef2493274e1d7e980a7e03209015
Courtney Solari <csolari@ufl.edu>
2019-07-07 15:50:39 -0500

    Fix issue of hover having opposite effect

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ const caption = document.getElementsByClassName("caption")[0];
 // Toggle .hide class
 const toggleHide = () => caption.classList.toggle("hide");
 
+// Hide caption when page loads
+caption.classList.add("hide");
+
 // When the user's mouse enters or leaves the image, toggle the display of the caption
 iceCream.addEventListener("mouseenter", toggleHide);
 iceCream.addEventListener("mouseleave", toggleHide);
